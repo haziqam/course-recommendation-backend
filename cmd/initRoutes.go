e88a6d3dfd7a609c9ce15e967c675a150443d259
Haziq <haziqabiyyu@gmail.com>
2023-12-25 22:51:25 +0700

    refactor: use controller term

--- cmd/initRoutes.go
+++ cmd/initRoutes.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/haziqam/course-scheduler-backend/packages/api"
+	"github.com/haziqam/course-scheduler-backend/packages/controller"
 )
 
 func initRoutes(app *fiber.App) {
@@ -10,20 +10,20 @@ func initRoutes(app *fiber.App) {
 		return c.SendString("Hello, masseh")
 	})
 
-	app.Get("/fakultas", api.GetAllFakultas)
-	app.Post("/fakultas", api.AddFakultas)
-	app.Delete("/fakultas", api.RemoveFakultas)
-	app.Post("/fakultas/addFromFile", api.AddFakultasFromFile)
+	app.Get("/fakultas", controller.GetAllFakultas)
+	app.Post("/fakultas", controller.AddFakultas)
+	app.Delete("/fakultas", controller.RemoveFakultas)
+	app.Post("/fakultas/addFromFile", controller.AddFakultasFromFile)
 
-	app.Get("/jurusan", api.GetAllJurusan)
-	app.Post("/jurusan", api.AddJurusan)
-	app.Delete("/jurusan", api.RemoveJurusan)
-	app.Post("/jurusan/addFromFile", api.AddJurusanFromFile)
+	app.Get("/jurusan", controller.GetAllJurusan)
+	app.Post("/jurusan", controller.AddJurusan)
+	app.Delete("/jurusan", controller.RemoveJurusan)
+	app.Post("/jurusan/addFromFile", controller.AddJurusanFromFile)
 
-	app.Get("/matkul", api.GetAllMatkul)
-	app.Post("/matkul", api.AddMatkul)
-	app.Delete("/matkul", api.RemoveMatkul)
-	app.Post("/matkul/addFromFile", api.AddMatkulFromFile)
-	app.Get("/matkul/find", api.FindMatkul)
-	app.Get("/matkul/find/bestOptions", api.FindBestOptions)
+	app.Get("/matkul", controller.GetAllMatkul)
+	app.Post("/matkul", controller.AddMatkul)
+	app.Delete("/matkul", controller.RemoveMatkul)
+	app.Post("/matkul/addFromFile", controller.AddMatkulFromFile)
+	app.Get("/matkul/find", controller.FindMatkul)
+	app.Get("/matkul/find/bestOptions", controller.FindBestOptions)
 }
