7f0dd819dea9546bd3ae45fa4f0b814fd26320c1
Haziq <haziqabiyyu@gmail.com>
2023-12-26 13:04:17 +0700

    refactor: move routes into a separate folder

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/haziqam/course-scheduler-backend/packages/database"
+	"github.com/haziqam/course-scheduler-backend/packages/routes"
 )
 
 func main() {
-    database.ConnectDB()
-    app := fiber.New()
-    app.Use(cors.New())
-    initRoutes(app)
-    app.Listen(":5000")
+	database.ConnectDB()
+	app := fiber.New()
+	app.Use(cors.New())
+	routes.InitRoutes(app)
+	app.Listen(":5000")
 }
---
+++ routes/routes.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/haziqam/course-scheduler-backend/packages/controllers"
+)
+
+func InitRoutes(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, masseh")
+	})
+
+	app.Get("/fakultas", controllers.GetAllFakultas)
+	app.Post("/fakultas", controllers.AddFakultas)
+	app.Delete("/fakultas", controllers.RemoveFakultas)
+	app.Post("/fakultas/addFromFile", controllers.AddFakultasFromFile)
+
+	app.Get("/jurusan", controllers.GetAllJurusan)
+	app.Post("/jurusan", controllers.AddJurusan)
+	app.Delete("/jurusan", controllers.RemoveJurusan)
+	app.Post("/jurusan/addFromFile", controllers.AddJurusanFromFile)
+
+	app.Get("/matkul", controllers.GetAllMatkul)
+	app.Post("/matkul", controllers.AddMatkul)
+	app.Delete("/matkul", controllers.RemoveMatkul)
+	app.Post("/matkul/addFromFile", controllers.AddMatkulFromFile)
+	app.Get("/matkul/find", controllers.FindMatkul)
+	app.Get("/matkul/find/bestOptions", controllers.FindBestOptions)
+}
